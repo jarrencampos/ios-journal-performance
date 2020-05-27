c39d60447011b780c4e587028c704457cb51ed59
Jarren Campos <jarrenmcampos@gmail.com>
2020-05-26 17:27:36 -0700

    Part 1. step 4

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -15,7 +15,7 @@ class CoreDataImporter {
     }
     
     func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
-        
+        print("Starting sync")
         self.context.perform {
             for entryRep in entries {
                 guard let identifier = entryRep.identifier else { continue }
@@ -27,6 +27,7 @@ class CoreDataImporter {
                     _ = Entry(entryRepresentation: entryRep, context: self.context)
                 }
             }
+            print("Sync finished")
             completion(nil)
         }
     }
